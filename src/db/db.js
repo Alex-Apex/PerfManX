cc1b6d6e02fbe4de29ad9605da105df1d22c1903
Alex Gomez <108309608+Alex-Apex@users.noreply.github.com>
2023-10-31 02:17:30 +0000

    feature: copied the db file

---
+++ src/db/db.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const sql = require('mssql');
+
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  server: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: false,
+    // change to true for local dev / self-signed certs
+  },
+};
+
+const appConnectionPoolPromise = new sql.ConnectionPool(config);
+
+module.exports = {
+  sql,
+  appConnectionPoolPromise,
+};
